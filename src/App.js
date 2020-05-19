4bbb34a03cbc1f60d633e55b6917eab136d199ba
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-19 17:09:14 -0600

    error cannot read 'imageURL' for card

--- src/App.js
+++ src/App.js
@@ -7,40 +7,51 @@ class App extends Component {
     super();
 
     this.state = {
-      pokemon: []
+      pokemon: [],
+      index: 0
     }
   }
 
   getPokemonCard = () => {
-    console.log("clicked")
-    axios.get(`https://api.pokemontcg.io/v1/cards`).then((pokemonCard) =>  { 
-      console.log("cards arrived")      
+    axios.get(`https://api.pokemontcg.io/v1/cards`).then((pokemonCard) =>  {     
       this.setState({
         pokemon: pokemonCard.data.cards
       });
-      console.log( "waiting for request")
     }).catch(error => alert("No Pokemon Found"))         
   }
 
+  buttonNext = () => {
+    let nextIndex = this.state.index + 1
+  
+    if(nextIndex === this.state.person.length) {
+      nextIndex = this.state.person.length -1
+    }
+    this.setState({index: nextIndex})
+  }
+   
+  buttonPrev = () => {
+    if(this.state.index === 0) 
+    return;
+  
+    this.setState(element => ({
+      index: element.index - 1
+    }))
+   
+  }
+
   render() {
-    const mappedCards = this.state.pokemon.map( card => {
-      return (
-        <div>
-           <img  src={card.imageUrl} alt={`${card.name}'s pokemon card`}/>
-        </div>
-      ) 
-    })
+    const { pokemon } = this.state
 
     return(
-      <div className="App">
-        <img className ="title" src= {"https://img.cinemablend.com/cb/b/1/e/2/4/3/b1e243d63dd84e8f971694725b5ba0163629e8322092e008fc646736f5d25479.jpg"} alt = {"pokemon logo"}/>
+      <div className ="App">
+        <img className ="title" src= "https://img.cinemablend.com/cb/b/1/e/2/4/3/b1e243d63dd84e8f971694725b5ba0163629e8322092e008fc646736f5d25479.jpg" alt = "pokemon logo"/>
         <div className ="card-container">
-          <h1 className= "card">{mappedCards}</h1>
+          <img className ="card" src={pokemon[0].imageUrl} alt ={`${pokemon[0].name} pokemon card`}/>
         </div>
-        <div className = "footer-buttons">
-          <button className="prev-next-buttons">{"<"}</button>
-          <button className="get-pokemon-button" onClick={this.getPokemonCard}>Get your Pokemon Card Here!</button>
-          <button className="prev-next-buttons">{">"}</button>
+        <div className ="footer-buttons">
+          <button className ="prev-next-buttons" onClick={this.buttonPrev}>{"<"}</button>
+          <button className ="get-pokemon-button" onClick={this.getPokemonCard}>Get your Pokemon Card Here!</button>
+          <button className ="prev-next-buttons" onClick={this.buttonNext}>{">"}</button>
         </div>
       </div>
     )
